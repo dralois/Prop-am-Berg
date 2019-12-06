6619feab58f17af8676a3f4ddb84f2faa66d96d0
dralois <epple.alexander@web.de>
2019-12-06 21:49:22 +0100

    Monitor Test Scene

---
+++ Assets/Scripts/Rendering/DisplayActivator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class DisplayActivator : MonoBehaviour
+{
+	private void Start()
+	{
+		Debug.Log($"Display count: {Display.displays.Length}");
+		foreach(Display curr in Display.displays)
+		{
+			curr.Activate();
+		}
+	}
+}
