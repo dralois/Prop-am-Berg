835559f574ff581d01bd7cb689e84256420b2bdf
dralois <epple.alexander@web.de>
2019-12-06 23:14:05 +0100

    Some reorganization

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour, InputSystem.IIngameActions
+{
+	public void OnJump(InputAction.CallbackContext context)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void OnLook(InputAction.CallbackContext context)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void OnMove(InputAction.CallbackContext context)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void OnPause(InputAction.CallbackContext context)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void OnSprint(InputAction.CallbackContext context)
+	{
+		throw new System.NotImplementedException();
+	}
+}
--- Assets/Scripts/Rendering/CameraManager.cs
+++ Assets/Scripts/Rendering/CameraManager.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
 
-	private InputSystem _defaultcontrols;
+	private InputSystem _controls;
 	private Vector2 _lookDelta;
 
-	private void Awake() => _defaultcontrols = new InputSystem();
+	private void Awake() => _controls = new InputSystem();
 
 	private void OnEnable()
 	{
 		// Control Scheme aktivieren
-		_defaultcontrols.Ingame.Enable();
+		_controls.Ingame.Enable();
 		// Delegate zuweisen
 		CinemachineCore.GetInputAxis = GetAxisCustom;
 	}
 	private void OnDisable()
 	{
 		// Control Scheme deaktivieren
-		_defaultcontrols.Ingame.Disable();
+		_controls.Ingame.Disable();
 		// Delegate entfernen
 		CinemachineCore.GetInputAxis = null;
 	}
@@ -27,7 +27,7 @@ public class CameraManager : MonoBehaviour
 	public float GetAxisCustom(string axisName)
 	{
 		// Delta holen
-		_lookDelta = _defaultcontrols.Ingame.Look.ReadValue<Vector2>();
+		_lookDelta = _controls.Ingame.Look.ReadValue<Vector2>();
 		_lookDelta.Normalize();
 		// Delta je nach Achse zurückgeben
 		if (axisName == "X")
