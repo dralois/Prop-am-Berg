56870519b4fb2fe88ba0a8d53ed4f891607396dc
VFelix <voelk_felix@web.de>
2019-12-07 00:00:42 +0100

    Update MainMenu.cs

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadLevelByIndex(int levelIndex)
     {
-        
+        SceneManager.LoadScene(levelIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadLevelByName(string levelName)
     {
-        
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
     }
 }
