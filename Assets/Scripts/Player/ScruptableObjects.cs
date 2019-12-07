208da2af6004480537eda385cbd04ab56343a39d
gazz0 <49281567+gazz0@users.noreply.github.com>
2019-12-07 14:57:54 +0100

    ModelChangeScript

---
+++ Assets/Scripts/Player/ScruptableObjects.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "NewObject", menuName = "Props/NewProp")]
+public class ScruptableObjects : ScriptableObject
+{
+    public GameObject _prop;
+    public Sprite _img;
+}
